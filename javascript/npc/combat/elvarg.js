544a0d0fc5ec363034df81dd8a4a27e3bdb0c741
Miika <miika.hagelberg@gmail.com>
2019-05-14 23:49:53 +0300

    Fixed quest completion teleport coordinates to new quest hut

--- javascript/npc/combat/elvarg.js
+++ javascript/npc/combat/elvarg.js
@@ -9,7 +9,7 @@ cs = new NCombatScript() {
     /* @Override */
     dropItemHook: function(player, dropTile, dropForIndex, hasRoWICharge) {
         player.getCombat().setDragonSlayer(true);
-        player.getMovement().teleport(3088, 3484, 0);
+        player.getMovement().teleport(3109, 3514, 0);
         player.getGameEncoder().sendMessage("<col=ff0000>You have completed Dragon Slayer!");
         player.getInventory().addOrDropItem(ItemID.COINS, 25000);
     }
