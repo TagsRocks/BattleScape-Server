1a1d344ab5bbd62079fe1757d96733ae502e850e
Miika <miika.hagelberg@gmail.com>
2019-06-01 21:01:01 +0300

    Added rest of teleport commands.

---
+++ src/script/packetdecoder/command/WestsCommand.java
@@ -0,0 +1,20 @@
+package script.packetdecoder.command;
+
+import com.palidino.osrs.io.Command;
+import com.palidino.osrs.model.player.Player;
+
+public class WestsCommand implements Command {
+
+    @Override
+    public boolean canUse(Player player) {
+        return !player.getController().inWilderness() && !player.getController().inPvPWorld()
+                && player.getController().canTeleport(false);
+    }
+
+    @Override
+    public void execute(Player player, String message) {
+        player.getMovement().teleport(2993, 3607);
+        player.getGameEncoder().sendMessage("You teleport to gorillas..");
+    }
+
+}
