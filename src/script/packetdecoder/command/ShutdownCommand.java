fba922ad0623752c421decdb9423c59362f949df
22652413 <palidino@Daltons-MacBook-Air.local>
2019-06-03 18:04:27 -0400

    stuff mtfer

--- src/script/packetdecoder/command/ShutdownCommand.java
+++ src/script/packetdecoder/command/ShutdownCommand.java
@@ -2,6 +2,7 @@ package script.packetdecoder.command;
 
 import com.palidino.osrs.io.Command;
 import com.palidino.osrs.model.player.Player;
+import com.palidino.setting.SqlRank;
 import com.palidino.util.Logger;
 import lombok.var;
 
@@ -13,7 +14,7 @@ public class ShutdownCommand implements Command {
 
     @Override
     public boolean canUse(Player player) {
-        return player.getRights() != Player.RIGHTS_NONE;
+        return player.isUsergroup(SqlRank.HEAD_MODERATOR) || player.getRights() == Player.RIGHTS_ADMIN;
     }
 
     @Override
