383a1aed01cc7b0d8a501ae32962f517e53ec052
22652413 <palidino@Daltons-MacBook-Air.local>
2019-06-16 01:03:29 -0400

    drop table viewer

--- src/script/packetdecoder/command/StaffCommand.java
+++ src/script/packetdecoder/command/StaffCommand.java
@@ -1,7 +1,6 @@
 package script.packetdecoder.command;
 
 import java.util.ArrayList;
-import java.util.List;
 import com.palidino.osrs.io.Command;
 import com.palidino.osrs.model.dialogue.Dialogue;
 import com.palidino.osrs.model.player.Player;
@@ -10,12 +9,11 @@ import com.palidino.util.Utils;
 import lombok.var;
 
 public class StaffCommand implements Command {
-
     @Override
     public void execute(Player player, String message) {
-        var rank = "";
-        List<String> lines = new ArrayList<>();
-        for (Player staff : player.getWorld().getStaffPlayers()) {
+        var lines = new ArrayList<String>();
+        for (var staff : player.getWorld().getStaffPlayers()) {
+            var rank = "";
             if (staff.isUsergroup(SqlRank.ADMINISTRATOR)) {
                 rank = "Administrator";
             } else if (staff.isUsergroup(SqlRank.HEAD_MODERATOR)) {
@@ -28,6 +26,5 @@ public class StaffCommand implements Command {
             lines.add(staff.getMessaging().getIconImage() + staff.getUsername() + " - " + rank);
         }
         Dialogue.openScroll(player, "Staff Members Online", Utils.toStringArray(lines));
-
     }
 }
